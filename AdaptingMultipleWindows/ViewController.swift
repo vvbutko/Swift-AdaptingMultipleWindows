5630acc758b60a659cbbe38449ba3f906a044cff
Vladimir Butko <vvbutko@gmail.com>
2022-01-16 10:39:42 -0500

    Initial Commit

---
+++ AdaptingMultipleWindows/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdaptingMultipleWindows
+//
+//  Created by Vladimir Butko on 2022-01-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
